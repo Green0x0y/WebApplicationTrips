228bf6c1915b9d6d8c765b52f060f9127348e237
Green0x0y <66325429+Green0x0y@users.noreply.github.com>
2023-01-15 11:47:58 +0100

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,34 @@
+
+export const environment = {
+  firebase: {
+    projectId: 'biuro-turystyczne-c34e7',
+    appId: '1:39447117269:web:76e8346eb79ec532c4d15c',
+    databaseURL: 'https://biuro-turystyczne-c34e7-default-rtdb.firebaseio.com',
+    storageBucket: 'biuro-turystyczne-c34e7.appspot.com',
+    apiKey: 'AIzaSyDAAN7y79wmKckCcIipj7AQciOZTiujl4Y',
+    authDomain: 'biuro-turystyczne-c34e7.firebaseapp.com',
+    messagingSenderId: '39447117269',
+    measurementId: 'G-2L2L55S18Q',
+  },
+    production: false,
+   
+    firebaseConfig : {
+        apiKey: "AIzaSyDAAN7y79wmKckCcIipj7AQciOZTiujl4Y",
+        authDomain: "biuro-turystyczne-c34e7.firebaseapp.com",
+        projectId: "biuro-turystyczne-c34e7",
+        storageBucket: "biuro-turystyczne-c34e7.appspot.com",
+        messagingSenderId: "39447117269",
+        appId: "1:39447117269:web:76e8346eb79ec532c4d15c",
+        measurementId: "G-2L2L55S18Q"
+      }
+};
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAAN7y79wmKckCcIipj7AQciOZTiujl4Y",
+  authDomain: "biuro-turystyczne-c34e7.firebaseapp.com",
+  projectId: "biuro-turystyczne-c34e7",
+  storageBucket: "biuro-turystyczne-c34e7.appspot.com",
+  messagingSenderId: "39447117269",
+  appId: "1:39447117269:web:76e8346eb79ec532c4d15c",
+  measurementId: "G-2L2L55S18Q"
+};
